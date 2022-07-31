aa914156b5009b242d689fbf995d16304410f660
Bhavya Shah <62424200+BhavyaShah1234@users.noreply.github.com>
2022-07-31 10:53:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests as r
+import threading
+import time
+
+
+def download_content(url, filename):
+    response = r.get(url)
+    if response.status_code == 200:
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+
+
+if __name__ == '__main__':
+    start = time.time()
+    download_content('https://source.unsplash.com/user/c_v_r/100x100', 'f.png')
+    end = time.time()
+    print(f'TIME TAKEN TO DOWNLOAD 1 IMAGE: {end - start}')
+    start = time.time()
+    for i in range(10):
+        thread = threading.Thread(target=download_content, args=['https://source.unsplash.com/user/c_v_r/100x100', f'{i}.png'])
+        thread.start()
+    end = time.time()
+    print(f'TIME TAKEN TO DOWNLOAD 10 IMAGE: {end - start}')
